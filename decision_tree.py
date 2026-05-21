d1dbe25cb64a369c71d924c524069260a9beefba
Marissa Liu <marissajliu@gmail.com>
2026-05-21 16:34:10 +0100

    first implementation of build_tree

--- decision_tree.py
+++ decision_tree.py
@@ -1,6 +1,7 @@
 import numpy as np
 from dataclasses import dataclass
-from typing import Union
+from typing import Union, NamedTuple
+from utils import gini_impurity, get_split_score
 
 
 @dataclass
@@ -12,21 +13,66 @@ class LeafNode:
 class InternalNode:
     feature: int
     threshold: float
-    left: Union['InternalNode', LeafNode]
-    right: Union['InternalNode', LeafNode]
+    left: Union["InternalNode", LeafNode]
+    right: Union["InternalNode", LeafNode]
+
+
+class Split(NamedTuple):
+    feature: int
+    threshold: float
+    score: float
+    row_mask: np.ndarray
 
 
 class RandomizedTree:
-    def __init__(self, max_depth: int = 10, min_samples_split: int = 2):
+    def __init__(self, max_depth: int = 10, min_samples: int = 2):
         self.max_depth = max_depth
-        self.min_samples_split = min_samples_split
+        self.min_samples = min_samples
         self.root = None
 
     def create(self, X: np.ndarray, Y: np.ndarray):
         self.root = self._build_tree(X, Y, depth=0)
-    
-    def _build_tree(self, X: np.ndarray, Y: np.ndarray, depth: int):
-        pass
+
+    def _majority(self, Y: np.ndarray) -> int:
+        return np.argmax(np.bincount(Y))
+
+    def _build_tree(
+        self, X: np.ndarray, Y: np.ndarray, depth: int, rng: np.random.Generator
+    ):
+        """
+        Returns root node of subtree
+
+        X: sample data of shape (n_examples, n_features)
+        Y: labels of shape (n_examples, )
+        """
+        assert len(X) > 0
+        assert len(X) == len(Y)
+        assert len(Y.shape) == 1
+
+        # Termination case
+        if np.all(X == X[0]) or depth == self.max_depth or len(X) == self.min_samples:
+            return LeafNode(label=self._majority(Y))
+
+        _, n_features = X.shape
+        features = rng.choice(np.range(n_features), np.sqrt(n_features), replace=False)
+        splits: list[Split] = []
+
+        for feat in features:
+            f_vals = X[:, feat]
+            f_min, f_max = np.min(f_vals), np.max(f_vals)
+            threshold = rng.choice(np.range(f_min, f_max))
+            row_mask = f_vals < threshold
+            score = get_split_score(Y, Y[row_mask], Y[~row_mask])
+            splits.append(
+                Split(feature=feat, threshold=threshold, score=score, row_mask=row_mask)
+            )
+
+        s = max(splits, key=lambda s: s.score)
+        left = self._build_tree(X[s.row_mask], Y[s.row_mask], depth + 1, rng)
+        right = self._build_tree(X[~s.row_mask], Y[~s.row_mask], depth + 1, rng)
+        return InternalNode(
+            feature=s.feature, threshold=s.threshold, left=left, right=right
+        )
 
     def predict(self, data: np.ndarray) -> int:
         """
@@ -40,7 +86,7 @@ class RandomizedTree:
 class RandomizedForest:
     def __init__(self, trees: list[RandomizedTree]):
         self.trees = trees
-    
+
     def initialize(num_trees: int) -> list[RandomizedTree]:
         """
         Factory function for initializing a forest of num_trees trees
---
+++ utils.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def gini_impurity(Y: np.ndarray):
+    if len(Y) == 0:
+        return 0.0
+    _, counts = np.unique(Y, return_counts=True)
+    p = counts / counts.sum()
+    return 1.0 - np.sum(p**2)
+
+
+def get_split_score(Y: np.ndarray, Y_left: np.ndarray, Y_right: np.ndarray):
+    n = len(Y)
+    n_left = len(Y_left)
+    n_right = len(Y_right)
+
+    if n_left == 0 or n_right == 0:
+        return 0.0
+
+    parent_imp = gini_impurity(Y)
+    left_imp = gini_impurity(Y_left)
+    right_imp = gini_impurity(Y_right)
+
+    left_weight = n_left / n
+    right_weight = n_right / n
+    weighted_split_imp = left_weight * left_imp + right_weight * right_imp
+
+    return parent_imp - weighted_split_imp
